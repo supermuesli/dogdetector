c9c94712841a1141d115b6483921ccbd44e963d9
supermuesli <art.lal@hotmail.com>
2020-10-26 04:14:47 +0100

    minibatch gd

--- client.py
+++ client.py
@@ -25,21 +25,22 @@ def main():
 
 	# create a CNN
 	net = CNN()
-	# load an existing model if possible
 	
+	# load an existing model if possible
 	net.load(model_path)
 
-	# works with a single path
-	sample = net.transform(dogs + '/3.jpg')
-	print(net(sample).mean())
-	
-	# works with list of paths (more efficient with batches)
-	sample = net.transform([(dogs + '/%d.jpg' % i) for i in range(665)])
-	print(net(sample).mean())
+	if sys.argv[1:] != []:
+		# works with a single path
+		sample = net.transform(sys.argv[1:])
+		print(net(sample).mean())
+	else:
+		# works with list of paths (more efficient with batches)
+		sample = net.transform([(dogs + '/%d.jpg' % i) for i in range(8000, 8665)])
+		print('mean sensitivity: ', net(sample).mean())
 
 
-	sample = net.transform([(cats + '/%d.jpg' % i) for i in range(665)])
-	print(net(sample).mean())
+		sample = net.transform([(cats + '/%d.jpg' % i) for i in range(8000, 8665)])
+		print('mean type I error: ', net(sample).mean())
 
 if __name__ == '__main__':
 	main()
--- ml.py
+++ ml.py
@@ -86,13 +86,15 @@ class CNN(nn.Module):
 		out_dim = 16
 		self.conv2 = nn.Conv2d(in_dim, out_dim, kernel_size)
 		self.fc1 = nn.Linear(out_dim * ((((im_size - kernel_size) // pool_size) - kernel_size) // pool_size)**2, 1200) # consider the forward function as to why this input dimension was chosen, also read here:
-		                                                                                                              # https://stackoverflow.com/questions/53784998/how-are-the-pytorch-dimensions-for-linear-layers-calculated
+		                                                                                                               # https://stackoverflow.com/questions/53784998/how-are-the-pytorch-dimensions-for-linear-layers-calculated
 		
 		self.fc2 = nn.Linear(1200, 84)
 		self.fc3 = nn.Linear(84, 1)
 
 		self.criterion = nn.MSELoss()
 		self.optimizer = optim.SGD(self.parameters(), lr=lr)
+
+		# https://pytorch.org/docs/stable/optim.html#torch.optim.lr_scheduler.ReduceLROnPlateau
 		self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min')
 
 		self.device = device
@@ -102,13 +104,18 @@ class CNN(nn.Module):
 		x = self.pool(F.relu(self.conv1(x)))
 		x = self.pool(F.relu(self.conv2(x)))
 		x = x.view(-1, self.fc1.in_features) # flatten the self.conv2 convolution layer. 
-		x = F.relu(self.fc1(x))
-		x = F.relu(self.fc2(x))
-		x = F.relu(self.fc3(x)) 
+		x = self.fc1(x)
+		x = self.fc2(x)
+		x = self.fc3(x) 
 		x = x.squeeze(-1)          # squeeze output into batch_dim
 
 		# squash value to interval [0, 1]. this works thanks to relu
-		x = x / (x.max() + 1)
+		min_val = abs(x.min())
+		max_val = x.max() + min_val
+		if max_val == 0:
+			max_val = 1
+		x = (x + min_val)/ max_val
+
 		return x
 
 	def save(self, path):
@@ -119,7 +126,7 @@ class CNN(nn.Module):
 		sd['im_size'] = self.im_size
 	
 		torch.save(sd, path + '.asd')
-
+		
 	def load(self, path):
 		sd = torch.load(path)
 		
@@ -174,7 +181,7 @@ class CNN(nn.Module):
 
 		return batch
 
-	def fit(self, cycles, training_loader):
+	def fit(self, cycles, training_loader, save_per_cycle=False):
 		""" Train the CNN for the given number of cycles on the given training dataset. 
 			
 		Args:
@@ -182,36 +189,36 @@ class CNN(nn.Module):
 			training_loader (torch.utils.data.DataLoader) : DataLoader containing the dataset.
 
 		"""
-		
-		for cycle in range(cycles):
+	
+		# the training loop utilizes mini-batch gradient descent
+
+		cycle = 0		
+		continue_training = True
+		while continue_training:
 			for batch in training_loader:
 				self.optimizer.zero_grad() # don't forget to zero the gradient buffers per batch !
 
-				if False:
-					choice = 'against batch'
-					self.loss = self.criterion(self(batch), torch.ones(batch.size()[0]))
+				# outputs
+				target = torch.ones(batch.size()[0])
 				
-				# randomly throw in black- and noise-tensors with 0 as output
-				else:
-					# outputs
-					target = torch.ones(batch.size()[0])
-					
-					for i in range(len(batch)):
-						if random.random() < 0.5:
-							for color_channel in range(len(batch[i])):
-								
-								for row in range(len(batch[i][color_channel])):
+				for i in range(len(batch)):
+					if random.random() < 0.7:
+						for color_channel in range(len(batch[i])):
 							
-									if random.random() < 0.5:
-										batch[i][color_channel][row] = torch.tensor([0 for i in range(self.im_size)])
-									else:
-										batch[i][color_channel][row] = torch.tensor([random.random() for i in range(self.im_size)])
+							for row in range(len(batch[i][color_channel])):
+						
+								if random.random() < 0.5:
+									# black 
+									batch[i][color_channel][row] = torch.tensor([0 for i in range(self.im_size)])
+								else:
+									# noise
+									batch[i][color_channel][row] = torch.tensor([random.random() for i in range(self.im_size)])
 
-									# row changes, output changes to 0
-									target[i] = torch.tensor([0])
-
-					choice = 'against batch/black/noise'
-					self.loss = self.criterion(self(batch), target)
+								# row changes, output changes to 0
+								target[i] = torch.tensor([0])
+				
+				choice = 'against batch/black/noise'
+				self.loss = self.criterion(self(batch), target)
 
 				# debugging loss
 				if cycle % 10 == 9:
@@ -224,6 +231,14 @@ class CNN(nn.Module):
 				                                      # read here:
 				                                      # https://github.com/pytorch/pytorch/pull/1370/commits/9f48a2cccb238aea13e2f170e60d48430e2b2aee
 
+				# cycle is finished at this point
+				if save_per_cycle:
+					self.save('model')
+
+				cycle += 1
+				if cycle == cycles:
+					continue_training = False
+					break
 
 	def test(self, testing_loader):
 		""" Test the CNN for the given testing dataset. 
@@ -259,9 +274,9 @@ def main():
 	# customize your datasource here
 	dogs = '/home/kashim/Downloads/dogsncats/dogs'
 	image_size = 115       # resize and (black-border)-pad images to image_size x image_size
-	data_ratio = 0.5     # only use the first 1% of the dataset
-	train_test_ratio = 0.6 # this would result in a 30:70 training:testing split
-	batch_size = 128        # for batch gradient descent set batch_size = int(len(data_total)*train_test_ratio*data_ratio)
+	data_ratio = 1.0       # only use the first 100% of the dataset
+	train_test_ratio = 0.6 # this would result in a 60:40 training:testing split
+	batch_size = 32        # for batch gradient descent set batch_size = int(len(data_total)*train_test_ratio*data_ratio)
 	data_total = ImageGrayScale(dogs, image_size)
 	#batch_size = int(len(data_total)*train_test_ratio*data_ratio)
 
@@ -278,19 +293,18 @@ def main():
 
 	# customize your CNN here
 	model_path = 'model.asd'
-	cycles = 100
+	cycles = 1000
 	learning_rate = 0.1
-
+	save_per_cycle = True
 
 	# create a CNN
 	net = CNN(im_size=image_size, lr=learning_rate)
 
 	# load an existing model if possible
-	
-	net.load(model_path)
+	#net.load(model_path)
 
 	# train the model
-	net.fit(cycles, training_loader)
+	net.fit(cycles, training_loader, save_per_cycle=save_per_cycle)
 
 	# test the model accuracy
 	#net.test(testing_loader)
